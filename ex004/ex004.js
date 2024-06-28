d58e648e2499f2b2c974893f1105442eed10a452
Nathalya Silva <nathalyagabrielly82@gmail.com>
2024-06-28 16:23:04 -0300

    dobro, triplo e raiz

---
+++ ex004/ex004.js
@@ -0,0 +1,9 @@
+let num = prompt('Digite um número: ');
+
+let dobro = parseInt(num) * 2;
+
+let triplo = parseInt(num) * 3;
+
+let raizQ = Math.sqrt(num);
+
+alert(`O dobro de ${num} é ${dobro}.\nO seu triplo é ${triplo}.\nE a raiz quadrada é ${raizQ}.`);
