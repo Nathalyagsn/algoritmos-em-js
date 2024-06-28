8a38dd79f651bd896693290b86099fe12d370a46
Nathalya Silva <nathalyagabrielly82@gmail.com>
2024-06-28 16:02:39 -0300

    atividades agora possuem seus respectivos nomes

---
+++ ex003/ex003.js
@@ -0,0 +1,9 @@
+let num = prompt('Digite um número: ')
+
+let sucessor = parseInt(num) + 1 ;
+
+let antecessor = parseInt(num) - 1;
+
+alert(`O sucessor de ${num} é ${sucessor} e seu antecessor é ${antecessor}`);
+
+
