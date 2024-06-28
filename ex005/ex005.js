7e73479a696edd711697a1fb5d601395c363d3f6
Nathalya Silva <nathalyagabrielly82@gmail.com>
2024-06-28 17:07:05 -0300

    conversor metros centimetros e milimetros

--- ex005/ex005.js
+++ ex005/ex005.js
@@ -1,6 +1,6 @@
-let nota1 = prompt('Qual a sua primeira nota? ')
-let nota2 = prompt('Digite sua segunda nota: ')
+let nota1 = prompt('Qual a sua primeira nota? ');
+let nota2 = prompt('Digite sua segunda nota: ');
 
 let media = (parseFloat(nota1) + parseFloat(nota2))/2;
 
-alert(`A sua média é: ${media}.`)
+alert(`A sua média é: ${media}.`);
---
+++ ex006/ex006.js
@@ -0,0 +1,6 @@
+let metros = prompt('Digite um valor em metros: ');
+
+let centimetros = parseInt(metros) * 100;
+let milimetros = parseInt(metros) * 1000;
+
+alert(`A conversão de ${metros} para centimetros é ${centimetros}.\nE milimetros é ${milimetros}.`);
