b91d27d07646e4327e3731012d4cfddd0b478beb
Nathalya Silva <nathalyagabrielly82@gmail.com>
2024-06-28 16:35:06 -0300

    média

---
+++ ex005/ex005.js
@@ -0,0 +1,6 @@
+let nota1 = prompt('Qual a sua primeira nota? ')
+let nota2 = prompt('Digite sua segunda nota: ')
+
+let media = (parseFloat(nota1) + parseFloat(nota2))/2;
+
+alert(`A sua média é: ${media}.`)
