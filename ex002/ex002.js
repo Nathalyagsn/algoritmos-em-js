c8fdbcd1e1cd47c4b83bd873f2b18946e049c3d2
Nathalya Silva <nathalyagabrielly82@gmail.com>
2024-06-28 15:48:00 -0300

    ex002

---
+++ ex002/ex002.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite um número: ');
+let num2 = prompt('Digite outro número: ');
+
+soma = (parseInt(num1) + parseInt(num2));
+
+alert(`A soma de ${num1} e ${num2} é igual a ${soma}. `);
